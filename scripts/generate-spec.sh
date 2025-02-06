4fb00e950b09d1d74011a292f541dce5a8df1e0f
Josh Holbrook <josh.holbrook@gmail.com>
2025-02-06 09:34:05 -0900

    Carry over changelog from spec

--- scripts/generate-spec.sh
+++ scripts/generate-spec.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 VERSION="${1}"
 RELEASE="${2}"
+CHANGELOG="$(./scripts/spec-changelog.py)"
 
 export VERSION
 export RELEASE
+export CHANGELOG
 
 gomplate -f ./plusdeck.spec.tmpl -o plusdeck.spec
