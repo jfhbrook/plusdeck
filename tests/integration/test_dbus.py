c7cba515923d5f2ac99eeca744a70f3cd13ae204
Josh Holbrook <josh.holbrook@gmail.com>
2025-04-08 12:00:51 -0800

    REALLY basic dbus test

---
+++ tests/integration/test_dbus.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+import pytest
+
+from tests.helpers import Cli
+
+
+def test_listen(dbus_cli, confirm) -> None:
+    with dbus_cli.bg("listen", quiet=False):
+        confirm("Mess with the Plusdeck. Are events showing up?")
+
+
+def test_listen_for(dbus_cli: Cli) -> None:
+    dbus_cli("listen", "--for", "1.0")
